f2fab474e819737fd6b91248f1fdc134654e8d0c
Mohamed Mehfoud <medm.bouh@gmail.com>
2022-07-25 13:17:12 +0900

    Sending the request everytime the appropriate values to the backend

--- src/actions/homeActions.js
+++ src/actions/homeActions.js
@@ -14,14 +14,24 @@ export const predictDataAction = (userDataHome) => async (dispatch) => {
     } else if (userDataHome["gender"] === "Male") {
       userDataHome["gender"] = 1
     }
+    console.log("User data home", userDataHome)
     const config = {
       headers: {
         "Content-Type": "application/json",
       },
     }
     const body = {
-      predictions: userDataHome,
+      age: userDataHome["age"],
+      gender: userDataHome["gender"],
+      spo2: userDataHome["SPO2"],
+      bmi: userDataHome["bmi"],
+      bp_dia: userDataHome["bp_dia"],
+      bp_sys: userDataHome["bp_sys"],
+      current_step: userDataHome["currentStep"],
+      pulse_rate: userDataHome["pulse_rate"],
+      temperature: userDataHome["temperature"],
     }
+
     const { data } = await axios.post(HOST + "predictions/", body, config)
 
     dispatch({
